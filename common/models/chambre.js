0cf8c2b7ba6c94815541b74da4241c9c241ff4b5
Abdoulaye Faye <a.faye5273@gmail.com>
2018-01-15 03:15:01 +0000

    ajout de nouvelles méthodes

--- common/models/chambre.js
+++ common/models/chambre.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Chambre) {
-
+  var app = require('../../server/server');
+  /**
+   * renvoient l'ensemble des positions dans une etage donnee
+   * @param {string} etage L'etage dans lequel on recherche
+   * @param {Function(Error, string)} callback
+   */
+  Chambre.prototype.positions = function(callback) {
+      Chambre.findById(this.id, {"include":["reservations"]}, function (err, chambre) {
+      var reservations = chambre.reservations();
+      var positions = reservations.map(function (r) {return r.position});
+      callback(null, positions);
+    })
+  };
 };
